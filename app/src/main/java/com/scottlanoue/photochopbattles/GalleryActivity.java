ecbc91d8279ecf7e638f6131a65b140f4f1c3e28
schott12521 <schott12521@gmail.com>
2015-09-25 19:50:04 -0400

    Still trying to make this menu work

--- app/src/main/java/com/scottlanoue/photochopbattles/GalleryActivity.java
+++ app/src/main/java/com/scottlanoue/photochopbattles/GalleryActivity.java
@@ -60,6 +60,7 @@ public class GalleryActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.gallery_menu, menu);
